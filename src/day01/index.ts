4c2233c3fcd4904ce81945088fd19764a48c46da
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2023-12-01 07:08:18 +0100

    day 1 solution

---
+++ src/day01/index.ts
@@ -0,0 +1,116 @@
+import run from "aocrunner";
+import * as R from "ramda";
+import {
+  readFile,
+  p,
+  pn,
+  trace,
+} from "../utils/index.js";
+
+const inputt = readFile(
+  "src/day01/input.txt",
+) as string;
+
+const test1 = `1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet`;
+
+const test2 = `two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen`;
+
+const findNumber = {
+  one: "1",
+  two: "2",
+  three: "3",
+  four: "4",
+  five: "5",
+  six: "6",
+  seven: "7",
+  eight: "8",
+  nine: "9",
+};
+
+type SolutionT = (input: string) => any;
+const part1: SolutionT = R.pipe(
+  p,
+  R.map(
+    R.pipe(
+      R.split(""),
+      R.applySpec({
+        a: R.find((x) => Number(x) == +x),
+        b: R.pipe(
+          R.reverse,
+          R.find((x) => Number(x) == +x),
+        ),
+      }),
+      R.juxt([R.prop("a"), R.prop("b")]),
+      R.apply(R.concat),
+      Number,
+    ),
+  ),
+  R.sum,
+);
+
+const part2: SolutionT = R.pipe(
+  p,
+  R.map(
+    R.applySpec({
+      a: (z) =>
+        R.match(
+          /[0-9]|zero|one|two|three|four|five|six|seven|eight|nine/g,
+          z,
+        )[0],
+      b: R.pipe(
+        R.reverse,
+        (z) =>
+          R.match(
+            /[0-9]|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin|orez/g,
+            z,
+          )[0],
+      ),
+    }),
+  ),
+  R.map(R.evolve({ b: R.reverse })),
+  R.map(R.juxt([R.prop("a"), R.prop("b")])),
+  R.map(R.map((x) => R.propOr(x, x, findNumber))),
+  R.map(R.join("")),
+  R.map(Number),
+  R.sum,
+);
+
+run({
+  part1: {
+    tests: [
+      {
+        input: test1,
+        expected: 142,
+      },
+      // {
+      //   input: inputt,
+      //   expected: 0,
+      // },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: test2,
+        expected: 281,
+      },
+      // {
+      //   input: inputt,
+      //   expected: 0,
+      // },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+});
