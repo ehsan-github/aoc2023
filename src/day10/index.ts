603efd46d6febfb7f57d6cd9f761b3242bee655e
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2023-12-15 20:38:41 +0100

    day 10 part 2

--- src/day10/index.ts
+++ src/day10/index.ts
@@ -42,23 +42,17 @@ const map = {
   ],
 };
 
-const test3 = `7-F7-
-.FJ|7
-SJLL7
-|F--J
-LJ.LJ`;
-const res3 = 8;
-
 const computeNextStep = ({
   s,
   currentPos,
   visited,
   routes,
   steps,
+  sIs,
 }: any) => {
   const [dy, dx] = [routes.length, routes[0].length];
   const [sY, sX] = R.isEmpty(currentPos) ? s : currentPos;
-  const possibleRoutes = [];
+  const possibleRoutes: any[] = [];
   const currPipe = map[routes[sY][sX]];
   const possiblePoints = currPipe
     .map(([y, x]) => [sY + y, sX + x])
@@ -79,6 +73,13 @@ const computeNextStep = ({
   const currPos = possibleRoutes.find(
     (p) => !visited.includes(p.join(",")),
   );
+
+  if (R.isEmpty(sIs)) {
+    sIs.push([currPos[0] - sY, currPos[1] - sX]);
+  }
+  if (!currPos) {
+    sIs.push([currentPos[0] - s[0], currentPos[1] - s[1]]);
+  }
   return {
     s,
     currentPos: currPos,
@@ -87,13 +88,11 @@ const computeNextStep = ({
       : visited,
     routes,
     steps: steps + 1,
+    sIs,
   };
 };
 
-type SolutionT = (input: string) => any;
-const part1: SolutionT = R.pipe(
-  p,
-  R.map(R.split("")),
+const findPoligon = R.pipe(
   (routes) => {
     const sY = routes.findIndex((x) => x.includes("S"));
     const sX = routes[sY].indexOf("S");
@@ -103,55 +102,99 @@ const part1: SolutionT = R.pipe(
       visited: [[sY, sX].join(",")],
       routes,
       steps: 0,
+      sIs: [],
     };
   },
   R.until(
     (obj) => obj.currentPos == undefined,
     computeNextStep,
   ),
+);
+
+type SolutionT = (input: string) => any;
+const part1: SolutionT = R.pipe(
+  p,
+  R.map(R.split("")),
+  findPoligon,
   R.prop("steps"),
   R.divide(R.__, 2),
   Math.ceil,
 );
 
-const part2: SolutionT = R.pipe(p);
+const opMap = {
+  J: "F",
+  F: "J",
+  L: "7",
+  7: "L",
+};
 
-const test2 = `.....
-.S-7.
-.|.|.
-.L-J.
-.....`;
-const res2 = 4;
+const isInside = (
+  routes: string[][],
+  visited: string[],
+  y: number,
+  x: number,
+) => {
+  let lines = 0;
+  let edges = [];
+  const row = routes[y];
+  for (let i = 0; i < x; i++) {
+    const char = row[i];
+    if (!visited.includes([y, i].join(","))) continue;
+    if (char == "|") {
+      lines++;
+      continue;
+    }
+    if (char == ".") continue;
+    if (char == "-") continue;
+    if (R.isEmpty(edges)) {
+      edges.push(char);
+    } else {
+      if (opMap[char] == R.last(edges)) {
+        lines++;
+      }
+      edges.pop();
+    }
+  }
+  const inside = lines % 2 == 1;
+  return inside;
+};
+
+const whatIsS = (sIs: number[][]) => {
+  const sortedSis = R.sortBy(R.prop(0), sIs)
+    .map(R.join(","))
+    .join("|");
+  return R.pipe(
+    R.toPairs,
+    R.map((x) => [
+      x[0],
+      R.sortBy(R.prop(0), x[1]).map(R.join(",")).join("|"),
+    ]),
+    R.find(([_sym, str]) => str === sortedSis),
+    R.head,
+  )(map);
+};
+
+const part2: SolutionT = R.pipe(
+  p,
+  R.map(R.split("")),
+  findPoligon,
+  ({ routes, visited, sIs, s }) => {
+    const S = whatIsS(sIs);
+    routes[s[0]][s[1]] = S;
+    let inDots = 0;
+    for (let i = 0; i < routes.length; i++) {
+      for (let j = 0; j < routes[i].length; j++) {
+        if (visited.includes([i, j].join(","))) continue;
+        if (isInside(routes, visited, i, j)) inDots++;
+      }
+    }
+    return inDots;
+  },
+);
 
 run({
-  part1: {
-    tests: [
-      // {
-      //   input: testInput,
-      //   expected: 8,
-      // },
-      // {
-      //   input: test2,
-      //   expected: res2,
-      // },
-      {
-        input: test3,
-        expected: res3,
-      },
-    ],
-    solution: part1,
-  },
   part2: {
-    tests: [
-      // {
-      //   input: testInput,
-      //   expected: 0,
-      // },
-      // {
-      //   input,
-      //   expected: 0,
-      // },
-    ],
+    tests: [],
     solution: part2,
   },
   trimTestInputs: true,
