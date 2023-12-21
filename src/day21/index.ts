cb4589ab4cf59df3dd1dfa71cec24dc45c9a52fa
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2023-12-21 17:21:25 +0100

    day 21: trivial part 2

--- src/day21/index.ts
+++ src/day21/index.ts
@@ -22,7 +22,42 @@ const testInput = `...........
 .##..##.##.
 ...........`;
 
-const getNeighbors = (board: string[][], pos: string) => {
+const commonSolution = (
+  st: number,
+  getN: Function,
+): SolutionT =>
+  R.pipe(
+    p,
+    R.map(R.split("")),
+    R.applySpec({
+      board: R.identity,
+      steps: R.always(0),
+      startingPoints: (p) => {
+        const rowIdx = R.findIndex(R.includes("S"), p);
+        const colIdx = R.findIndex(
+          R.includes("S"),
+          p[rowIdx],
+        );
+        return [`${rowIdx}, ${colIdx}`];
+      },
+    }),
+    R.until(R.where({ steps: R.equals(st) }), (all) =>
+      R.evolve({
+        steps: R.inc,
+        startingPoints: R.pipe(
+          R.reduce((acc, item) => {
+            const neighbors = getN(all.board, item);
+            return [...acc, ...neighbors];
+          }, []),
+          R.uniq,
+        ),
+      })(all),
+    ),
+    R.prop("startingPoints"),
+    R.length,
+  );
+
+const getNeighbors1 = (board: string[][], pos: string) => {
   const [y, x] = pos.split(",").map(Number);
   const neighbors = [];
   if (y > 0 && board[y - 1][x] !== "#")
@@ -35,60 +70,80 @@ const getNeighbors = (board: string[][], pos: string) => {
     neighbors.push(`${y}, ${x + 1}`);
   return neighbors;
 };
+const part1 = commonSolution(6, getNeighbors1);
 
-const part1: SolutionT = R.pipe(
-  p,
-  R.map(R.split("")),
-  R.applySpec({
-    board: R.identity,
-    steps: R.always(0),
-    startingPoints: (p) => {
-      const rowIdx = R.findIndex(R.includes("S"), p);
-      const colIdx = R.findIndex(
-        R.includes("S"),
-        p[rowIdx],
-      );
-      return [`${rowIdx}, ${colIdx}`];
-    },
-  }),
-  R.until(R.where({ steps: R.equals(64) }), (all) =>
-    R.evolve({
-      steps: R.inc,
-      startingPoints: R.pipe(
-        R.reduce((acc, item) => {
-          const neighbors = getNeighbors(all.board, item);
-          return [...acc, ...neighbors];
-        }, []),
-        R.uniq,
+// part2
+const getFromMap = (
+  dir: number[],
+  board: string[][],
+  y: number,
+  x: number,
+) => {
+  const [dy, dx] = dir;
+  const [ny, nx] = [y + dy, x + dx];
+  const h = board.length;
+  const w = board[0].length;
+  const boardY = R.cond([
+    [R.lte(h), R.modulo(R.__, h)],
+    [
+      R.gt(0),
+      R.pipe(
+        R.modulo(R.__, h),
+        R.unless(R.equals(-0), R.add(h)),
+        Math.abs,
       ),
-    })(all),
-  ),
-  R.prop("startingPoints"),
-  R.length,
-);
+    ],
+    [R.T, R.identity],
+  ])(ny);
 
-const part2: SolutionT = R.pipe(p);
+  const boardX = R.cond([
+    [R.lte(w), R.modulo(R.__, w)],
+    [
+      R.gt(0),
+      R.pipe(
+        R.modulo(R.__, w),
+        R.unless(R.equals(-0), R.add(w)),
+        Math.abs,
+      ),
+    ],
+    [R.T, R.identity],
+  ])(nx);
+  const value = board[boardY][boardX];
+  if (value !== "#") return [ny, nx].join(",");
+  return null;
+};
+
+const getNeighbors2 = (board: string[][], pos: string) => {
+  const [y, x] = pos.split(",").map(Number);
+  const up = getFromMap([-1, 0], board, y, x);
+  const down = getFromMap([1, 0], board, y, x);
+  const left = getFromMap([0, -1], board, y, x);
+  const right = getFromMap([0, 1], board, y, x);
+  return [up, down, left, right].filter(R.identity);
+};
+
+const part2 = commonSolution(10, getNeighbors2);
 
 run({
   part1: {
     tests: [
       // {
       //   input: testInput,
-      //   expected: 42,
+      //   expected: 16,
       // },
       // {
       //   input,
-      //   expected: 0,
+      //   expected: 3649,
       // },
     ],
     solution: part1,
   },
   part2: {
     tests: [
-      // {
-      //   input: testInput,
-      //   expected: 0,
-      // },
+      {
+        input: testInput,
+        expected: 0,
+      },
       // {
       //   input,
       //   expected: 0,
@@ -97,5 +152,5 @@ run({
     solution: part2,
   },
   trimTestInputs: true,
-  onlyTests: false,
+  onlyTests: true,
 });
