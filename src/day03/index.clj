7cc8ad5f9d0d5168f98812c500f40b64559108d9
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2024-05-05 01:05:24 +0200

    remove extra spaces

--- src/day03/index.clj
+++ src/day03/index.clj
@@ -3,7 +3,6 @@
 
 (def input (slurp "src/day03/input.txt"))
 
-
 (def test "467..114..
 ...*......
 ..35..633.
@@ -15,11 +14,9 @@
 ...$.*....
 .664.598..")
 
-(defn is-digit? [d] (let [v (- (int (apply char d)) 48)]
-                      (and (<= 0 v) (>= 9 v))))
+(defn is-digit? [d] (let [v (- (int (apply char d)) 48)] (and (<= 0 v) (>= 9 v))))
 
-(defn update-valid-nums [c]
-  (fn [vs] (conj (vec (drop-last 1 vs)) (+ (Integer/parseInt c) (* 10 (last vs))))))
+(defn update-valid-nums [c] (fn [vs] (conj (vec (drop-last 1 vs)) (+ (Integer/parseInt c) (* 10 (last vs))))))
 
 (defn get-adj [idx len] (for [i (range (max 0 (- idx 1)) (min len (+ idx 2)))] i))
 
@@ -40,7 +37,6 @@
                         all-adj (for [a adj
                                       l (filter some? [prev-line line next-line])] (get l a))
                         valid (<= 1 (count (filter (fn [x] (and (not (is-digit? x)) (not= "." x))) all-adj)))]
-
                     (if active-valid
                       (update acc :valid-nums (update-valid-nums c))
                       (if (< 0 active-num)
@@ -50,13 +46,11 @@
                                        (if valid
                                          (assoc x :valid-nums (conj (vec valid-nums) new-active) :active-num new-active :active-valid true)
                                          (assoc x :active-num new-active))))))
-
                         (-> acc
                             (assoc
                              :active-valid valid
                              :active-num (Integer/parseInt c)
                              :valid-nums (if valid (conj valid-nums (Integer/parseInt c)) valid-nums))))))
-
                   (assoc acc :active-valid false :active-num 0)))
               {:prev-line prev-line :next-line next-line :active-valid false :valid-nums [] :active-num 0}
               (map-indexed vector line)))) x)))
