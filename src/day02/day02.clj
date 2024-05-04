370f6fabda8763dc94fc5f087aa11494775e9a6e
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2024-05-04 20:20:13 +0200

    clj d2-p2

--- src/day02/day02.clj
+++ src/day02/day02.clj
@@ -3,16 +3,31 @@
 
 (def input (slurp "src/day02/input.txt"))
 
+(defn common-part [input]
+  (->> input
+       (str/split-lines)
+       (map #(str/split % #": "))
+       (map (fn [[g v]]
+              (let [[_ n] (str/split g #" ")
+                    k (->> v
+                           ((fn [x] (str/split x #"; |, ")))
+                           (map #(str/split % #" "))
+                           (reduce (fn [acc [v name]] (assoc acc name (max (Integer/parseInt v) (get acc name 0)))) {}))]
+                [(Integer/parseInt n) k])))))
+
+;; part-1
 (->> input
-     (str/split-lines)
-     (map #(str/split % #": "))
-     (map (fn [[g v]]
-            (let [[_ n] (str/split g #" ")
-                  k (->> v
-                         ((fn [x] (str/split x #"; |, ")))
-                         (map #(str/split % #" "))
-                         (reduce (fn [acc [v name]] (assoc acc name (max (Integer/parseInt v) (get acc name 0)))) {}))]
-              [(Integer/parseInt n) k])))
+     common-part
      (filter (fn [[_ k]] (and (>= 12 (get k "red")) (>= 13 (get k "green")) (>= 14 (get k "blue")))))
      (map first)
+     (reduce +))
+
+;; part-2
+(->> input
+     common-part
+     (map #(->>
+            %
+            second
+            ((fn [k] (map (fn [j] (get k j 0)) ["red" "green" "blue"])))
+            (reduce *)))
      (reduce +))
