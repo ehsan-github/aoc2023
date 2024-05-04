f0a30b10175602b9150c23d593e4e2e4e2a58b21
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2024-05-04 19:24:41 +0200

    clj-d1-p2

--- src/day01/index.clj
+++ src/day01/index.clj
@@ -3,11 +3,6 @@
 
 (def input (slurp "src/day01/input.txt"))
 
-(def test "1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet")
-
 (let [zero (int \0)
       nine (int \9)]
   (defn is-digit? [c] (<= zero (int c) nine)))
@@ -23,20 +18,37 @@ treb7uchet")
   (first (filter f (reverse coll))))
 
 
-(defn part-1 [input]
-  (->> input
-       str/split-lines
-       (map #(apply vector %))
-       (map #(let [first (find-first is-digit? %)
-                   last (find-last is-digit? %)
-                   comb (str/join [first last])]
-               (Integer/parseInt comb)))
-       (reduce +)))
-
-(part-1 input)
-     
-
-     
-    
+;; part-1
+(->> input
+     str/split-lines
+     (map #(apply vector %))
+     (map #(let [first (find-first is-digit? %)
+                 last (find-last is-digit? %)
+                 comb (str/join [first last])]
+             (Integer/parseInt comb)))
+     (reduce +))
 
 
+;; part-2
+(def find-number
+  {"one" "1",
+   "two" "2",
+   "three" "3",
+   "four" "4",
+   "five" "5",
+   "six" "6",
+   "seven" "7",
+   "eight" "8",
+   "nine" "9"})
+
+(->> input
+     str/split-lines
+     (map (fn [x]
+            (let [a (re-find #"[0-9]|zero|one|two|three|four|five|six|seven|eight|nine" x)
+                  b-r (re-find #"[0-9]|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin|orez" (apply str (reverse x)))
+                  b (apply str (reverse b-r))
+                  a-num (get find-number a a)
+                  b-num (get find-number b b)
+                  res (Integer/parseInt (str a-num b-num))]
+              res)))
+     (reduce +))
